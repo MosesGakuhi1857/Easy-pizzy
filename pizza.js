74789fdf183602163ae146da689b3a507e39b715
MosesGakuhi1857 <king'orimoses386@gmail.com>
2021-08-01 01:38:44 +0300

    intial javascript commit

---
+++ pizza.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+
+    $(".pick-delivery").click(function () {
+        let pizzaName = $("#pizza option:selected").val();
+        let crust = $(" #crust option:selected").val();
+        let toppings = $("#toppings option:selected").val();
+        let size = $("#size option:selected").val();
+        let quantity = $("#quantity option:selected").val();
+        console.log(pizzaName);
+        console.log(crust);
+    });
+
+    // let price, totalPrice;
+    // switch (flavour) {
+    //     case flavour = "vegtikka":
+    //         switch (size) {
+    //             case size = "regular":
+    //                 price = 300;
+    //                 if (crust === "thin") {
+    //                     totalPrice = (price * number) + 100;
+    //                 } else if (crust === "thick") {
+    //                     totalPrice = (price * number) + 150;
+    //                 } else if (crust === "flatbread") {
+    //                     totalPrice = (price * number) + 180;
+    //                 } else {
+    //                     totalPrice = (price * number) + 280;
+    //                 }
+    //                 break;
+    //             case size = "medium":
+    //                 price = 600;
+    //                 if (crust === "thin") {
+    //                     totalPrice = (price * number) + 100;
+    //                 } else if (crust === "thick") {
+    //                     totalPrice = (price * number) + 150;
+    //                 } else if (crust === "flatbread") {
+    //                     totalPrice = (price * number) + 180;
+    //                 } else {
+    //                     totalPrice = (price * number) + 280;
+    //                 }
+    //                 break;
+    //             case size = "large":
+    //                 price = 1200;
+    //                 if (crust === "thin") {
+    //                     totalPrice = (price * number) + 100;
+    //                 } else if (crust === "thick") {
+    //                     totalPrice = (price * number) + 150;
+    //                 } else if (crust === "flatbread") {
+    //                     totalPrice = (price * number) + 180;
+    //                 } else {
+    //                     totalPrice = (price * number) + 280;
+    //                 }
+    //                 break;
+    //         }
+    //         break;
+
+    let price, totalPrice;
+    switch (pizzaName) {
+        case pizzaName = "BIANCA":
+            switch (size) {
+                case
+
+            }
+
+
+    }
+
+
+
+
+
+
+    //             if (crust === "thin") {
+    //                 totalPrice = (price * number) + 100;
+    //             } else if (crust === "thick") {
+    //                 totalPrice = (price * number) + 150;
+    //             } else if (crust === "flatbread") {
+    //                 totalPrice = (price * number) + 180;
+    //             } else {
+    //                 totalPrice = (price * number) + 280;
+    //             }
+    //             break;
+
+
+
+});
